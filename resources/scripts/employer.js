722e4f900f08d7700f148bbaf397bf6a2c790658
Katie Mulholland <katiemulholland@MacBook-Pro-9.local>
2020-12-02 21:17:39 -0600

    worked on validate

--- resources/scripts/employer.js
+++ resources/scripts/employer.js
@@ -1,25 +1,31 @@
-function getEmployers(){
+function getEmployers()
+{
     //const allEmployersApiUrl = "https://localhost:5001/api/employers";
     const allEmployersApiUrl = "https://crimsonsnacksapi.herokuapp.com/api/employers";
 
-    fetch(allEmployersApiUrl).then(function(response){
+    fetch(allEmployersApiUrl).then(function (response)
+    {
         console.log(response);
         return response.json();
-    }).then(function(json){
+    }).then(function (json)
+    {
         let html = "<ul>";
-        json.forEach((employer)=>{
-            html += "<li>" + employer.userName + employer.password + "</li>" + "<button onclick=\"deleteEmployer("+employer.employerID+")\">Delete</button>";
+        json.forEach((employer) =>
+        {
+            html += "<li>" + employer.userName + employer.password + "</li>" + "<button onclick=\"deleteEmployer(" + employer.employerID + ")\">Delete</button>";
         });
         html += "</ul>";
         document.getElementById("employers").innerHTML = html;
-    }).catch(function(error){
+    }).catch(function (error)
+    {
         console.log(error);
     });
 }
 
-function postEmployer(){
-  //const postEmployerApiUrl = "https://localhost:5001/api/employers";
-  const postEmployerApiUrl = "https://crimsonsnacksapi.herokuapp.com/api/employers";
+function postEmployer()
+{
+    //const postEmployerApiUrl = "https://localhost:5001/api/employers";
+    const postEmployerApiUrl = "https://crimsonsnacksapi.herokuapp.com/api/employers";
     const EmployerUserName = document.getElementById("UserName").value;
     console.log(EmployerUserName);
     const EmployerPassword = document.getElementById("Password").value;
@@ -36,39 +42,68 @@ function postEmployer(){
             Password: EmployerPassword
         })
     })
-    .then((response)=>{
-        console.log(response);
-        getEmployers();
-    })
+        .then((response) =>
+        {
+            console.log(response);
+            getEmployers();
+        })
 }
 
-function validate(UserName, Password){
-   //const checkEmployersApiUrl = "https://localhost:5001/api/employers";
-   const checkEmployersApiUrl = "https://crimsonsnacksapi.herokuapp.com/api/employers";
+function validate(UserName, Password)
+{
+    //const checkEmployersApiUrl = "https://localhost:5001/api/employers";
+    const checkEmployersApiUrl = "https://crimsonsnacksapi.herokuapp.com/api/employers";
 
-    fetch(checkEmployersApiUrl).then(function(response){
+    fetch(checkEmployersApiUrl).then(function (response)
+    {
         console.log(response);
         return response.json();
-    }).then(function(json){
-        json.forEach((employer)=>{
+    }).then(function (json)
+    {
+        let found = false;
+        json.forEach((employer) =>
+        {
             console.log(employer.userName);
             console.log(employer.password);
             console.log(UserName);
             console.log(Password);
-        if(employer.userName == UserName && employer.password == Password){
-           location.href='employerchoice.html';
-           throw BreakException;
-        }
-        else{
-           location.href='employerloginpage.html';
-        }
-    });
-    }).catch(function(error){
+            if (employer.userName == UserName && employer.password == Password)
+            {
+
+                found = true;
+                //location.href='clock.html';
+                //document.cookie = "EmpID="+EmpID+";";
+
+            }
+            if (found)
+            {
+                location.href = 'employerchoice.html';
+            }
+            else
+            {
+                location.href = 'employee.html';
+            }
+            // json.forEach((employer)=>{
+            //     console.log(employer.userName);
+            //     console.log(employer.password);
+            //     console.log(UserName);
+            //     console.log(Password);
+            // if(employer.userName == UserName && employer.password == Password){
+            //    location.href='employerchoice.html';
+            //    throw BreakException;
+            // }
+            // else{
+            //    location.href='employerloginpage.html';
+            // }
+        });
+    }).catch(function (error)
+    {
         console.log(error);
     });
 }
 
-deleteEmployer = function(id){
+deleteEmployer = function (id)
+{
     console.log(id);
     //const deleteEmployerApiUrl = "https://localhost:5001/api/employers/" + id;
     const deleteEmployerApiUrl = "https://crimsonsnacksapi.herokuapp.com/api/employers" + id;
@@ -80,8 +115,9 @@ deleteEmployer = function(id){
             "Content-Type": 'application/json'
         }
     })
-    .then((response)=>{
-        console.log(response);
-        getEmployers();
-    })
+        .then((response) =>
+        {
+            console.log(response);
+            getEmployers();
+        })
 }
